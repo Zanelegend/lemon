f271a9f71d7e1eb906e7456f1b0b2e44ec383d53
Giancarlo Buomprisco <giancarlopsk@gmail.com>
2023-11-06 15:33:28 +0100

    0.7.12 (#104)
    * 0.7.12
    
    1. Added organization leave/delete features
    2. Added account delete
    3. Fixed oAuth issue when invited
    4. Improved auth callback error handling
    5. Added a dedicated password reset page
    6. Added tests

--- src/configuration.ts
+++ src/configuration.ts
@@ -36,8 +36,18 @@ const configuration = {
   },
   production,
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
-  enableThemeSwitcher: true,
   theme: Themes.Light,
+  features: {
+    enableThemeSwitcher: true,
+    enableAccountDeletion: getBoolean(
+      process.env.NEXT_PUBLIC_ENABLE_ACCOUNT_DELETION,
+      false,
+    ),
+    enableOrganizationDeletion: getBoolean(
+      process.env.NEXT_PUBLIC_ENABLE_ORGANIZATION_DELETION,
+      false,
+    ),
+  },
   paths: {
     signIn: '/auth/sign-in',
     signUp: '/auth/sign-up',
@@ -132,3 +142,11 @@ const configuration = {
 };
 
 export default configuration;
+
+function getBoolean(value: unknown, defaultValue: boolean) {
+  if (typeof value === 'string') {
+    return value === 'true';
+  }
+
+  return defaultValue;
+}
